e0f3a5c46cf75d85a2f56cd4b17fc1e2a8af3be5
Your New Username <sahupravin960@gmail.com>
2024-08-17 16:41:30 +0530

    added github-recent-activity project

--- github-recent-activity/github.js
+++ github-recent-activity/github.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import { Octokit } from "octokit";
 const orgs = process.argv;
+<<<<<<< HEAD
 const octokit = new Octokit({});
+=======
+const octokit = new Octokit({
+  auth: process.env.GIT_TOKEN,
+});
+>>>>>>> 5335d93 (added github-recent-activity project)
 
 async function fetchActivityData() {
   try {
