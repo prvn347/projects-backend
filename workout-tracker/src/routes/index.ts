c8bd9e3553efea3b5119f19b91f1d4a2cc698bc2
Your New Username <sahupravin960@gmail.com>
2024-08-31 13:06:25 +0530

    add:swagger-ui

--- workout-tracker/src/routes/index.ts
+++ workout-tracker/src/routes/index.ts
@@ -2,9 +2,14 @@ import { Application } from "express";
 import userRoutes from "./user";
 import workRoutes from "./workout";
 import healthRoutes from "./health";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load("docs/spec.yml");
 
 export const initializeRoutes = (app: Application) => {
   console.log("hi from initialization route");
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use("/user", userRoutes);
   app.use("/workout", workRoutes);
   app.use("/health", healthRoutes);
